9f03b48e3a6f5b1bd88963761fb5c7f7f6789438
mungase-narayan <narayanmungase03@gmail.com>
2025-05-20 21:23:26 +0530

    add: graph lec. love babbar

--- 77-Graph/77.graphStoreInListFormat.cpp
+++ 77-Graph/77.graphStoreInListFormat.cpp
@@ -1,34 +1,39 @@
 // Graph Store in List Format
 #include <iostream>
-#include<vector>
+#include <vector>
 using namespace std;
 
-void addEdge(vector<int> adj[], int u, int v) {
+void addEdge(vector<int> adj[], int u, int v)
+{
   adj[u].push_back(v);
   adj[v].push_back(u);
 }
 
 // Print the graph
-void printGraph(vector<int> adj[], int V) {
-  for (int i = 0; i < V; ++i) {
-    cout << "Vertex "<< i << " :";
-    for (auto x : adj[i]){
+void printGraph(vector<int> adj[], int V)
+{
+  for (int i = 0; i < V; ++i)
+  {
+    cout << "Vertex " << i << " :";
+    for (auto x : adj[i])
+    {
       cout << " => " << x;
     }
-    cout<<endl;
+    cout << endl;
   }
 }
 
-int main() {
-  int V = 5;
+int main()
+{
+  int v = 5;
 
   // Create a graph
-  vector<int> adj[V];
+  vector<int> adj[v];
 
   // Add edges
   addEdge(adj, 0, 1);
   addEdge(adj, 0, 2);
   addEdge(adj, 0, 3);
   addEdge(adj, 1, 2);
-  printGraph(adj, V);
+  printGraph(adj, v);
 }
